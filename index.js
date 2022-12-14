d3d110eee2efad345494c064ccce144255d4a81c
Paras Garg <parasgarg@microsoft.com>
2022-12-14 01:49:58 -0800

    initialized firebase app

---
+++ index.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+//
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+//
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDRdYH9jXagr2WiMGU7wqqKLcp9mpqbs5g",
+//     authDomain: "spicy-sweet-sour.firebaseapp.com",
+//       projectId: "spicy-sweet-sour",
+//         storageBucket: "spicy-sweet-sour.appspot.com",
+//           messagingSenderId: "568559133103",
+//             appId: "1:568559133103:web:bd6a8ede5203ae9ee94f5a",
+//               measurementId: "G-QFZ8TP9MBV"
+//               };
+//
+//               // Initialize Firebase
+//               const app = initializeApp(firebaseConfig);
+//               const analytics = getAnalytics(app);
